3915051db6ddca2db521eff2d271c8bfe6e61996
Felipe Hoffmann <felipejh@gmail.com>
2022-02-26 02:30:44 -0300

    feat: add signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React, { ReactElement } from 'react';
+import { Text, View } from 'react-native';
+
+function SignIn(): ReactElement {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+export default SignIn;
