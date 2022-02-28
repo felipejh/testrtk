88311ec0c4eb0da425a4ee1aa806b547413a69f3
Felipe Hoffmann <felipejh@gmail.com>
2022-02-28 03:22:30 -0300

    refactor: improve eslint rules

--- App.tsx
+++ App.tsx
@@ -11,10 +11,6 @@ import Routes from '~/routes';
 // Stores
 import { store, persistor } from '~/config/store/create-store';
 
-if (__DEV__) {
-  import('~/config/ReactotronConfig').then(() => console.log('Reactotron Configured'));
-}
-
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,29 +2,21 @@ import { useLoginMutation } from '~/store/api/auth.api';
 import { setCredentials } from '~/store/slice/auth.slice';
 
 // Packages
-import React, { ReactElement, useState, useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { ReactElement, useState } from 'react';
 import { ActivityIndicator, Alert } from 'react-native';
 
 // Stores
-import { useAppDispatch, useTypedSelector } from '~/config/store/create-store';
+import { useAppDispatch } from '~/config/store/create-store';
 
 // Styles
 import { Container, Input, Button, ButtonText } from './styles';
 
 function SignIn(): ReactElement {
-  const navigation = useNavigation();
   const dispatch = useAppDispatch();
 
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [login, { isLoading, isError }] = useLoginMutation();
-
-  useEffect(() => {
-    if (isError) {
-      console.error('Ocorreu um erro');
-    }
-  }, [isError]);
+  const [login, { isLoading }] = useLoginMutation();
 
   const handleSignIn = async (): Promise<void> => {
     const params = { email, password };
@@ -38,7 +30,7 @@ function SignIn(): ReactElement {
         dispatch(setCredentials({ name: Name, email: Email, token: Token }));
       }
     } catch (error) {
-      console.tron.log(error);
+      Alert.alert('Ops', `Ocorreu um erro: ${error}`);
     }
   };
 
